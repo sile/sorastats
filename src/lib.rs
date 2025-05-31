313925a0b35c2a847aefc472882337e9d1500c7b
Takeru Ohta <phjgt308@gmail.com>
2025-05-31 18:37:14 +0900

    Remove clap dependency and implement custom argument parsing with noargs

--- src/lib.rs
+++ src/lib.rs
@@ -7,47 +7,13 @@ pub mod poll;
 pub mod stats;
 pub mod ui;
 
-#[derive(Debug, Clone, clap::Parser)]
+#[derive(Debug, Clone)]
 pub struct Options {
-    /// 「Sora の API の URL（リアルタイムモード）」あるいは「過去に `--record` で記録したファイルのパス（リプレイモード）」
     pub sora_api_url: String,
-
-    /// 統計 API から情報を取得する間隔（秒単位）
-    #[clap(long, short = 'i', default_value = "1")]
     pub polling_interval: std::num::NonZeroUsize,
-
-    /// チャートの X 軸の表示期間（秒単位）
-    #[clap(long, short = 'p', default_value = "60")]
     pub chart_time_period: std::num::NonZeroUsize,
-
-    /// 集計対象に含めるコネクションをフィルタするための正規表現
-    ///
-    /// コネクションの各統計値は "${KEY}:${VALUE}" という形式の文字列に変換された上で、
-    /// 指定の正規表現にマッチ（部分一致）するかどうかがチェックされる。
-    /// 一つでもマッチする統計値が存在する場合には、そのコネクションは集計対象に含まれる。
-    ///
-    /// 例えば、チャンネル名が "sora" のコネクションのみを対象にしたい場合には
-    /// "^channel_id:sora$" という正規表現を指定すると良い。
-    #[clap(long, short = 'c', default_value = ".*:.*")]
     pub connection_filter: regex::Regex,
-
-    /// 集計対象に含める統計項目をフィルタするための正規表現
-    ///
-    /// 指定された正規表現にマッチ（部分一致）する統計項目のみが表示される。
-    ///
-    /// 例えば、 RTP 関連の統計情報のみを対象としたい場合には
-    /// "^rtp[.]" という正規表現を指定すると良い。
-    #[clap(long, short = 'k', default_value = ".*")]
     pub stats_key_filter: regex::Regex,
-
-    /// 指定されたファイルに、取得した統計情報を記録する
-    ///
-    ///
-    /// `<SORA_API_URL>`引数に URL の代わりにこのファイルへのパスを指定することで、
-    /// 記録した統計情報を後から閲覧することができる
-    ///
-    /// リプレイモードの場合には、このオプションを指定しても無視される
-    #[clap(long)]
     pub record: Option<PathBuf>,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,33 @@
-use clap::Parser;
 use orfail::OrFail;
 use sorastats::{poll, ui};
 use std::path::PathBuf;
 
-/// WebRTC SFU Sora の統計情報ビューア
-#[derive(Debug, Parser)]
-#[clap(version)]
+#[derive(Debug)]
 struct Args {
-    #[clap(flatten)]
     options: sorastats::Options,
-
-    #[clap(hide = true, long)]
     logfile: Option<PathBuf>,
-
-    #[clap(hide = true,long, default_value_t = simplelog::LevelFilter::Info)]
     loglevel: simplelog::LevelFilter,
-
-    #[clap(hide = true, long)]
     truncate_log: bool,
 }
 
 impl Args {
-    fn parse_with_noargs() -> noargs::Result<Self> {
+    fn parse() -> noargs::Result<Self> {
         let mut args = noargs::raw_args();
 
-        // Set metadata for help
         args.metadata_mut().app_name = env!("CARGO_PKG_NAME");
         args.metadata_mut().app_description = "WebRTC SFU Sora の統計情報ビューア";
 
-        // Handle well-known flags
         if noargs::VERSION_FLAG.take(&mut args).is_present() {
             println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
             std::process::exit(0);
         }
         noargs::HELP_FLAG.take_help(&mut args);
 
-        // Parse options fields (from sorastats::Options)
         let sora_api_url: String = noargs::arg("<SORA_API_URL>")
-        .doc("「Sora の API の URL（リアルタイムモード）」あるいは「過去に `--record` で記録したファイルのパス（リプレイモード）」")
-        .take(&mut args)
-        .then(|a| a.value().parse())?;
+            .doc("「Sora の API の URL（リアルタイムモード）」あるいは「過去に `--record` で記録したファイルのパス（リプレイモード）」")
+            .example("http://localhost:5000/api")
+            .take(&mut args)
+            .then(|a| a.value().parse())?;
 
         let polling_interval: std::num::NonZeroUsize = noargs::opt("polling-interval")
             .short('i')
@@ -57,24 +45,47 @@ impl Args {
 
         let connection_filter: regex::Regex = noargs::opt("connection-filter")
             .short('c')
-            .doc("集計対象に含めるコネクションをフィルタするための正規表現")
+            .doc(concat!(
+                "集計対象に含めるコネクションをフィルタするための正規表現\n",
+                "\n",
+                "コネクションの各統計値は '${KEY}:${VALUE}' という形式の文字列に変換された上で、\n",
+                "指定の正規表現にマッチ（部分一致）するかどうかがチェックされる。\n",
+                "一つでもマッチする統計値が存在する場合には、そのコネクションは集計対象に含まれる。\n",
+                "\n",
+                "例えば、チャンネル名が 'sora' のコネクションのみを対象にしたい場合には\n",
+                "'^channel_id:sora$' という正規表現を指定すると良い。\n",
+
+            ))
             .default(".*:.*")
             .take(&mut args)
             .then(|o| regex::Regex::new(o.value()))?;
 
         let stats_key_filter: regex::Regex = noargs::opt("stats-key-filter")
             .short('k')
-            .doc("集計対象に含める統計項目をフィルタするための正規表現")
+            .doc(concat!(
+                "集計対象に含める統計項目をフィルタするための正規表現\n",
+                "\n",
+                "指定された正規表現にマッチ（部分一致）する統計項目のみが表示される。\n",
+                "\n",
+                "例えば、 RTP 関連の統計情報のみを対象としたい場合には\n",
+                "'^rtp[.]' という正規表現を指定すると良い。\n",
+            ))
             .default(".*")
             .take(&mut args)
             .then(|o| regex::Regex::new(o.value()))?;
 
         let record: Option<PathBuf> = noargs::opt("record")
-            .doc("指定されたファイルに、取得した統計情報を記録する")
+            .doc(concat!(
+                "指定されたファイルに、取得した統計情報を記録する\n",
+                "\n",
+                "`<SORA_API_URL>`引数に URL の代わりにこのファイルへのパスを指定することで、\n",
+                "記録した統計情報を後から閲覧することができる\n'",
+                "\n",
+                "リプレイモードの場合には、このオプションを指定しても無視される\n"
+            ))
             .take(&mut args)
             .present_and_then(|o| o.value().parse())?;
 
-        // Parse hidden Args fields
         let logfile: Option<PathBuf> = noargs::opt("logfile")
             .take(&mut args)
             .present_and_then(|o| o.value().parse())?;
@@ -109,7 +120,7 @@ impl Args {
 }
 
 fn main() -> noargs::Result<()> {
-    let args = Args::parse();
+    let args = Args::parse()?;
 
     setup_logger(&args).or_fail()?;
 
